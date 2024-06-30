1a8a1aad4dd5d843a7edba240a585c3a16dc527b
irfan septian <ivanseftian79@gmai.com>
2024-06-30 23:53:40 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import SearchBar from "./components/SearchBar";
 function App() {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4">Pokedex</h1>
+      <h1 className="text-3xl font-bold mb-4">Pikadex</h1>
       <SearchBar />
       <PokemonList />
     </div>
