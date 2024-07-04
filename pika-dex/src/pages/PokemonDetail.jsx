ca40ff6ab8dcbafed66c840e23a25f2e17608895
irfan septian <ivanseftian79@gmai.com>
2024-07-04 21:16:01 +0700

    update detailPage for styling and add data from api

--- pika-dex/src/pages/PokemonDetail.jsx
+++ pika-dex/src/pages/PokemonDetail.jsx
@@ -33,196 +33,80 @@ function PokemonDetail() {
   }, [pokemonName, navigate]);
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <div className="text-center text-gray-500">Loading...</div>;
   }
 
   if (error) {
-    return <div>Error: {error}</div>;
+    return <div className="text-center text-red-500">Error: {error}</div>;
   }
 
+  const getStatLabel = (statName) => {
+    switch (statName) {
+      case "hp":
+        return "HP";
+      case "attack":
+        return "Att";
+      case "defense":
+        return "Def";
+      case "special-attack":
+        return "Sp.Atk";
+      case "special-defense":
+        return "Sp.Def";
+      case "speed":
+        return "Speed";
+      default:
+        return statName;
+    }
+  };
+
   return (
-    <div
-      className="rounded-3xl shadow-2xl p-16 text-center flex justify-space-x-2 m-16 gap-16"
-      style={{
-        background: "rgb(239, 239, 237)",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "24px",
-        }}
-      >
-        <img
-          src={pokemonData.sprites.front_default}
-          alt={pokemonData.name}
-          className="w-52 h-52 mx-auto"
-          style={{
-            border: "1px dashed rgb(90, 64, 145)",
-            borderRadius: "10px",
-          }}
-        />
-        <h2
-          className="text-3xl font-bold text-center mb-4"
-          style={{ color: "rgb(90, 64, 145)" }}
-        >
-          {pokemonData.name}
-        </h2>
-      </div>
-      <div className="grid grid-cols-4 gap-8">
-        <div
-          style={{
-            background: "white",
-            borderRadius: "10px",
-            padding: "24px",
-            width: "160px",
-            height: "160px",
-          }}
-        >
-          <h2
-            style={{
-              backgroundColor: "rgb(90, 64, 145)",
-              borderRadius: "8px",
-              padding: "4px",
-              color: "white",
-              marginBottom: "8px",
-            }}
-            className="font-semibold"
-          >
-            Type
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center p-4 md:p-16 ">
+      <div className="bg-white rounded-3xl shadow-2xl p-8 md:p-16 text-center md:flex md:justify-between w-full max-w-4xl">
+        {/* Image and Basic Info */}
+        <div className="mb-8 md:mb-0 ">
+          <img
+            src={pokemonData.sprites.front_default}
+            alt={pokemonData.name}
+            className="w-48 h-48 md:w-52 md:h-52 mx-auto border border-dashed border-purple-600 rounded-md"
+          />
+          <h2 className="text-2xl md:text-3xl font-bold text-center my-4 text-purple-600">
+            {pokemonData.name}
           </h2>
-          <ul style={{ marginTop: "18px" }}>
-            {pokemonData.types.map((type) => (
-              <li
-                style={{
-                  color: "rgb(170, 131, 87)",
-                  fontWeight: "600",
-                  border: "1px dashed rgb(170, 131, 87)",
-                  padding: "4px",
-                }}
-                key={type.type.name}
-              >
-                {type.type.name}
-              </li>
-            ))}
-          </ul>
+          <p className="text-gray-600">#{pokemonData.id}</p>
         </div>
 
-        <div
-          style={{
-            background: "white",
-            borderRadius: "10px",
-            padding: "24px",
-            width: "160px",
-            height: "auto",
-          }}
-        >
-          <h2
-            style={{
-              backgroundColor: "rgb(90, 64, 145)",
-              borderRadius: "8px",
-              padding: "4px",
-              color: "white",
-              marginBottom: "8px",
-            }}
-            className="font-semibold"
-          >
-            Abilities
-          </h2>
-          <ul style={{ marginTop: "18px" }}>
-            {pokemonData.abilities.map((ability) => (
-              <li
-                style={{
-                  color: "rgb(170, 131, 87)",
-                  fontWeight: "600",
-                  border: "1px dashed rgb(170, 131, 87)",
-                  padding: "4px",
-                }}
-                key={ability.ability.name}
-              >
-                {ability.ability.name}
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <div
-          style={{
-            background: "white",
-            borderRadius: "10px",
-            padding: "24px",
-            width: "160px",
-            height: "160px",
-          }}
-        >
-          <h2
-            style={{
-              backgroundColor: "rgb(90, 64, 145)",
-              borderRadius: "8px",
-              padding: "4px",
-              color: "white",
-              marginBottom: "8px",
-            }}
-            className="font-semibold"
-          >
-            Weight
-          </h2>
-          <ul style={{ marginTop: "18px" }}>
-            {pokemonData.abilities.map((ability) => (
-              <li
-                style={{
-                  color: "rgb(170, 131, 87)",
-                  fontWeight: "600",
-                  border: "1px dashed rgb(170, 131, 87)",
-                  padding: "4px",
-                }}
-                key={ability.ability.name}
-              >
-                {ability.ability.name}
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <div
-          style={{
-            background: "white",
-            borderRadius: "10px",
-            padding: "24px",
-            width: "160px",
-            height: "160px",
-          }}
-        >
-          <h2
-            style={{
-              backgroundColor: "rgb(90, 64, 145)",
-              borderRadius: "8px",
-              padding: "4px",
-              color: "white",
-              marginBottom: "8px",
-            }}
-            className="font-semibold"
-          >
-            Height
-          </h2>
-          <ul style={{ marginTop: "18px" }}>
-            {pokemonData.abilities.map((ability) => (
-              <li
-                style={{
-                  color: "rgb(170, 131, 87)",
-                  fontWeight: "600",
-                  border: "1px dashed rgb(170, 131, 87)",
-                  padding: "4px",
-                }}
-                key={ability.ability.name}
-              >
-                {ability.ability.name}
-              </li>
-            ))}
-          </ul>
+        {/* Detailed Stats */}
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-4 md:gap-8  ">
+          {[
+            { title: "Type", data: pokemonData.types.map((t) => t.type.name) },
+            {
+              title: "Abilities",
+              data: pokemonData.abilities.map((a) => a.ability.name),
+            },
+            { title: "Weight", data: [`${pokemonData.weight} kg`] },
+            { title: "Height", data: [`${pokemonData.height} dm`] },
+            { title: "Base Exp", data: [pokemonData.base_experience] },
+            {
+              title: "Stats",
+              data: pokemonData.stats.map(
+                (s) => `${getStatLabel(s.stat.name)}: ${s.base_stat}`
+              ),
+            },
+          ].map(({ title, data }) => (
+            <div key={title} className="bg-cyan-50 rounded-md p-4 shadow-md ">
+              <h3 className="text-purple-600 font-semibold bg-purple-100 rounded-md py-1 px-2 mb-2">
+                {title}
+              </h3>
+              <ul className=" list-inside font-mono list-none text-neutral-500 ">
+                {" "}
+                {data.map((item, index) => (
+                  <li key={index} className="text-amber-800">
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
       </div>
     </div>
